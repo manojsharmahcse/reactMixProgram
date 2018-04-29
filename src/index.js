de86f478110c800ad07dce07d9b12354f3720c19
manojsharmahcse <manojsharmahcse@gmail.com>
2018-04-29 17:50:55 +0530

    link_router

--- src/index.js
+++ src/index.js
@@ -4,9 +4,27 @@ import './index.css';
 import App from './App';
 import Toggle from './Toggle';
 import Tabs from './Tabs';
-import index from './index';
+
 
 import registerServiceWorker from './registerServiceWorker';
+import index from './index';
 
-ReactDOM.render(<Tabs /> , document.getElementById('root'));
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+ReactDOM.render(
+	<Router>
+		<div>
+
+			<div>
+				<Link to='/'>Home</Link>&nbsp;
+				<Link to='/tabs'>Tab</Link>&nbsp;
+				<Link to='/toggle'>Toggle</Link>
+			</div>
+
+			<Route exact component={App} path="/" />
+			<Route component={Tabs} path="/tabs" />
+			<Route component={Toggle} path="/Toggle" />
+
+		</div>
+	 </Router>, document.getElementById('root'));
 registerServiceWorker();
