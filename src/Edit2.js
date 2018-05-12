fd70ffaa901944e5416eaa6eeca4505bfd0e15c7
manoj sharma <manojsharmahcse@gmail.com>
2018-05-12 09:43:22 +0000

    Update Edit2.js

--- src/Edit2.js
+++ src/Edit2.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 export default class Users extends Component {
 	constructor(props) {
+	    
+	    
+	    
+	    
+	    
 		super(props);
 
 		this.state = {
